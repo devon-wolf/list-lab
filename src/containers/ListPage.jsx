3e88ed5c73b9ada4d36abf2b4196090fd2aa27e4
Devon Wolfkiel <devonwolf@tutanota.com>
2021-05-11 17:11:36 -0700

    Implement the API fetch

--- src/containers/ListPage.jsx
+++ src/containers/ListPage.jsx
@@ -1,22 +1,30 @@
 import React, { Component } from 'react';
 import style from './ListPage.css';
 import VillagerList from '../components/displays/VillagerList';
+import { fetchVillagers } from '../services/fetchVillagers';
 
 export default class ListPage extends Component {
+	state = {
+		loading: true,
+		villagers: [],
+	}
+
+	componentDidMount = async () => {
+		const response = await fetchVillagers();
+		this.setState({ villagers: response, loading: false });
+	}
+
 	render() {
 		return (
 			<div className={style.listPage}>
 				<h2>Villagers</h2>
-				<VillagerList 
-					villagers={[
-						{
-							id: 1,
-							name: 'test',
-							image: 'fake',
-							phrase: 'sure'
-						}
-					]}
-				/>
+				{this.state.loading
+					? 	<p>Loading!</p>
+					: 	<VillagerList 
+						villagers={this.state.villagers}
+						/>
+				}
+				
 			</div>
 		)
 	}
---
+++ src/services/fetchVillagers.js
@@ -0,0 +1,11 @@
+export const fetchVillagers = async () => {
+	const response = await fetch('https://ac-vill.herokuapp.com/villagers');
+	const results = await response.json();
+
+	return results.map(villager => ({
+		id: villager._id,
+		name: villager.name,
+		image: villager.image,
+		phrase: villager.phrase
+	}));
+}
